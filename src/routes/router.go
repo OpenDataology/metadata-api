5aec59821154a84b230309014de315d7d062395d
Clement Li <lizi4@huawei.com>
2022-03-03 18:52:07 +0800

    update license and searching

--- src/routes/router.go
+++ src/routes/router.go
@@ -16,11 +16,13 @@ func InitRouter() *gin.Engine {
 		v1.GET("/dataset", basic.ListDatasets)
 		v1.GET("/get_license_basic_by_id", basic.GetLicenseBasicById)
 		v1.GET("/get_license_basic_by_name", basic.GetLicenseBasicByName)
+		v1.GET("/search_license_basic_by_name", basic.SearchLicenseBasicByName)
 		v1.GET("/get_license_data_by_id", basic.GetLicenseDataById)
 		v1.GET("/get_license_model_by_id", basic.GetLicenseModelById)
 		v1.GET("/get_license_other_by_id", basic.GetLicenseOtherById)
 		v1.GET("/get_dataset_by_id", basic.GetDatasetById)
 		v1.GET("/get_dataset_by_name", basic.GetDatasetByName)
+		v1.GET("/search_dataset_by_name", basic.SearchDatasetByName)
 
 	}
 
